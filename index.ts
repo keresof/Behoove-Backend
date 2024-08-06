687369b5fcc31aa6fcd7b54d3095a38054095dfb
Kerem Çelik <keremcelik@Kerem-MacBook-Air-2.local>
2024-08-06 13:56:35 +0300

    render connection try part 2

--- index.ts
+++ index.ts
@@ -1,21 +1,27 @@
 import contentRoutes from "./modules/content/routes/contentRoutes";
+import dotenv from 'dotenv';
+dotenv.config();
 
-require('dotenv').config();
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import authRoutes from './modules/user/routes/authRoutes';
-import userRoutes from './modules/user/routes/userRoutes';
 import bearerMiddleware from './middleware/bearerMiddleware';
 import passport from 'passport';
 import './infra/passportConfig';
-import './infra/redisConfig';
+import { createClient } from 'redis';
 import connectDB from './infra/db';
 import cors from 'cors';
 import refreshToken from './modules/user/models/refreshToken';
 import { CLEAR_EXPIRED_TOKENS_INTERVAL } from './utilities/constants';
 
 const PORT = process.env.PORT || 3030;
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
+
+// Redis Client Setup
+const redisClient = createClient({ url: REDIS_URL });
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.connect().catch(console.error);
 
 // Swagger configuration
 const swaggerOptions = {
@@ -56,34 +62,41 @@ export const configureApp = (middleware?: any[]) => {
 
 const app = configureApp([bearerMiddleware]);
 
-// Existing Swagger documentation...
-
-// Use the routes
-// app.use('/api', userRoutes);
-
 app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/content', contentRoutes);
 
-if (!process.env.NODE_ENV || (process.env.NODE_ENV && process.env.NODE_ENV !== 'test')) {
-    app.listen(PORT, () => {
-        connectDB();
-        // clear expired refresh tokens on startup and setup interval to clear them periodically
-        // use iife to avoid top-level await
-        (async () => {
-            console.log('Clearing expired tokens...');
-            await refreshToken.deleteExpired();
-            console.log('Expired tokens cleared');
-            console.log(`Setting up interval to clear expired tokens every ${CLEAR_EXPIRED_TOKENS_INTERVAL} seconds`);
-            setInterval(async () => {
+const startServer = async () => {
+    try {
+        await connectDB();
+        console.log('Connected to MongoDB');
+
+        await redisClient.ping();
+        console.log('Connected to Redis');
+
+        app.listen(PORT, () => {
+            console.log(`Express app running on port ${PORT}`);
+            console.log(`Swagger documentation available at http://localhost:${PORT}/api-docs`);
+
+            // Clear expired tokens
+            (async () => {
                 console.log('Clearing expired tokens...');
                 await refreshToken.deleteExpired();
                 console.log('Expired tokens cleared');
-            }, CLEAR_EXPIRED_TOKENS_INTERVAL! * 1000);
-        })();
-        console.log(`Express app running on port ${PORT}`);
-        console.log(`Swagger documentation available at http://localhost:${PORT}/api-docs`);
-    });
-}
+                console.log(`Setting up interval to clear expired tokens every ${CLEAR_EXPIRED_TOKENS_INTERVAL} seconds`);
+                setInterval(async () => {
+                    console.log('Clearing expired tokens...');
+                    await refreshToken.deleteExpired();
+                    console.log('Expired tokens cleared');
+                }, CLEAR_EXPIRED_TOKENS_INTERVAL! * 1000);
+            })();
+        });
+    } catch (error) {
+        console.error('Failed to start the server:', error);
+        process.exit(1);
+    }
+};
+
+startServer();
 
 export default app;
